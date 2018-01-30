1514fb064feee7453e29560cefdb7dfd3ff1dc00
kratorr <31060213+kratorr@users.noreply.github.com>
2018-01-30 21:54:51 +0300

    Woking version

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,16 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding="utf-8") as f:
+        raw_data = json.load(f)
+        return raw_data
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps((data), sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    print(pretty_print_json(load_data(sys.argv[1])))
