35049b4c04f0e83510917bad21e95c587eeb8fd9
kratorr <31060213+kratorr@users.noreply.github.com>
2018-02-01 10:59:41 +0300

    fixed values name, function load_data cleaned

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,21 @@
 import json
 import sys
-
+import os
 
 def load_data(filepath):
-    with open(filepath, "r", encoding="utf-8") as f:
-        raw_data = json.load(f)
-        return raw_data
+    with open(filepath, "r", encoding="utf-8") as json_file:
+        decoded_json = json.load(json_file)
+        return decoded_json
 
 
-def pretty_print_json(data):
-    return json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
+def pretty_print_json(json_object):
+    print(json.dumps(json_object, sort_keys=True, indent=4, ensure_ascii=False))
 
 
-if __name__ == '__main__':
-    print(pretty_print_json(load_data(sys.argv[1])))
+if __name__ == "__main__":
+    try:
+        filepath = sys.argv[1]
+        data_for_print = load_data(filepath)
+        pretty_print_json(data_for_print)
+    except:
+        raise FileNotFoundError
